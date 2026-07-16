15e8e5461634986c675b924c321aa94819464e4f
sudarshan.kapali <sudarshankapalisk@gmail.com>
2026-07-16 17:54:40 +0545

    first commit

--- src/main/java/org/example/testapp/TestAppApplication.java
+++ src/main/java/org/example/testapp/TestAppApplication.java
@@ -8,6 +8,7 @@ public class TestAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestAppApplication.class, args);
+        System.out.println("Application started");
 	}
 
 }
